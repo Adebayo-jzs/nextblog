a0b7a87a92073bd627712e706bb1c83ea8100acd
Adebayo-jzs <adedejiadebayo732@gmail.com>
2025-12-23 00:45:12 +0100

    generateStaticParams for seo

--- app/post/[slug]/page.jsx
+++ app/post/[slug]/page.jsx
@@ -1,26 +1,36 @@
 import { supabase } from "@/utils/supabase";
-export default async function BlogPost({ params }) {
-    const {slug} = await params;
 
-    const {data: post, error } = await supabase
-        .from("posts")
-        .select("*")
-        .eq("slug", slug)
-        .single();
-    if(error || !post){
-        return <div className="flex items-center justify-center min-h-screen ">Post not found</div>;
-    }
 
-    return (
-        <article className="mx-auto max-w-3xl py-10">
-            <h1 className="mb-4 text-3xl font-bold">{post.title}</h1>
-            <p className="text-zinc-600 dark:text-zinc-400">
-                {new Date(post.created_at).toLocaleDateString()}
-            </p>
+export async function generateStaticParams() {
+  const { data: posts, error } = await supabase
+    .from("posts")
+    .select("slug");
 
-            <div className="prose dark:prose-invert mt-6">
-                {post.content}
-            </div>
-        </article>
-    );
+  if (error || !posts) return [];
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function BlogPostPage({ params }) {
+  const { slug } = await  params;
+
+  const { data: post } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("slug", slug)
+    .single();
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <article className="mx-auto max-w-3xl py-10">
+      <h1 className="text-3xl font-bold">{post.title}</h1>
+      <span className="text-sm text-zinc-500 dark:text-zinc-400">{post.created_at}</span>
+      <div className="mt-6">{post.content}</div>
+    </article>
+  );
 }
