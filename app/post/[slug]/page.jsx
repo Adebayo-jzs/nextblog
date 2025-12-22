fe4871dd20fa21062fe5472808f3a092a13c69ef
Adebayo-jzs <adedejiadebayo732@gmail.com>
2025-12-23 00:21:38 +0100

    dynamic routes handling

--- app/post/[slug]/page.jsx
+++ app/post/[slug]/page.jsx
@@ -1,1 +1,27 @@
 import { supabase } from "@/utils/supabase";
+
+export default async function BlogPost({ params }) {
+    const {slug} = await params;
+
+    const {data: post, error } = await supabase
+        .from("posts")
+        .select("*")
+        .eq("slug", slug)
+        .single();
+    if(error || !post){
+        return <div>Post not found</div>;
+    }
+
+    return (
+        <article className="mx-auto max-w-3xl py-10">
+            <h1 className="mb-4 text-3xl font-bold">{post.title}</h1>
+            <p className="text-zinc-600 dark:text-zinc-400">
+                {new Date(post.created_at).toLocaleDateString()}
+            </p>
+
+            <div className="prose dark:prose-invert mt-6">
+                {post.content}
+            </div>
+        </article>
+    );
+}
