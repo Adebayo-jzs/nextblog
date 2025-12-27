0f824a26331ba1618bb4f274b5fea34e1f8b794b
Adebayo-jzs <adedejiadebayo732@gmail.com>
2025-12-27 13:09:55 +0100

    seo optimisation

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -38,6 +40,7 @@ export default function RootLayout({ children }) {
           </main>
         {/* </div> */}
       </div>
+      <Analytics />
       </body>
     </html>
   );
