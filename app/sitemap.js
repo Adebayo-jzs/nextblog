e82b842c743fa4d56d3d3a3b472717f55ce8bec7
Adebayo Adedeji <adedejiadebayo732@gmail.com>
2025-12-31 04:26:55 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,20 +1,10 @@
- 
-import { createClient } from "@/lib/client";
-export default async function sitemap() {
-  const supabase = createClient()
-  const baseUrl = 'https://blog.theebayo.name.ng';
-  const { data: posts } = await supabase
-    .from("posts")
-    .select("slug");
+import { MetadataRoute } from "next";
 
-  const postUrls = posts.map((post) => ({
-    url: `${baseUrl}/posts/${post.slug}`,
-    lastModified: new Date(),
-    priority:0.8,
-  }));
-
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: baseUrl, lastModified: new Date(),priority:1, },
-    ...postUrls,
+    {
+      url: "https://blog.theebayo.name.ng",
+      lastModified: new Date(),
+    },
   ];
 }
